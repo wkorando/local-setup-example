5e67b2ce642d20f438408b26dd4c899b76e09c97
Billy Korando <bkorando@keyholesoftware.com>
2017-12-22 14:35:19 -0600

    Updating contracts

---
+++ start-usda/src/main/java/com/bk/start/StartUsdaApplication.java
@@ -0,0 +1,12 @@
+package com.bk.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUsdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUsdaApplication.class, args);
+	}
+}
